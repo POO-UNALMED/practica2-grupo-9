c685a81d150c7d5e07deafebb1f003da7b576be4
MauricioChavarria <47341821+MauricioChavarria@users.noreply.github.com>
2020-10-25 11:49:59 -0500

    se crearon los metodos de cliente pero falta terminarlos

--- src/gestorAplicacion/finanzas/Cliente.java
+++ src/gestorAplicacion/finanzas/Cliente.java
@@ -5,9 +5,37 @@ import java.util.List;
 
 public class Cliente {
 	private int edad;
-	private int nivel_afiliacion;
+	private static int nivel_afiliacion;
 	private String metodo_pago;
 	private List<Factura> facturas = new ArrayList<Factura> ();
+	
+	
+	public void aumentarAfiliacion() {
+		Cliente.nivel_afiliacion+=1;
+	}
+	
+	public void disminuirAfiliacion() {
+		Cliente.nivel_afiliacion-=1;
+	}
+	
+	public void descuentoAfiliacion() {
+		if (Cliente.getNivel_afiliacion()==1) {
+			//hacer descuento
+		}
+		else if (Cliente.getNivel_afiliacion()==2) {}
+			//hacer descuento
+	}
+	
+	public void descuentoEdad(int ed) {
+		if (ed >= 60) {
+			
+		}
+	}
+	
+	public void descuentoMetodoPago(String metodo) {
+		
+	}
+	
 	public List<Factura> getFacturas() {
 		return facturas;
 	}
@@ -20,11 +48,11 @@ public class Cliente {
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-	public int getNivel_afiliacion() {
+	public static int getNivel_afiliacion() {
 		return nivel_afiliacion;
 	}
 	public void setNivel_afiliacion(int nivel_afiliacion) {
-		this.nivel_afiliacion = nivel_afiliacion;
+		Cliente.nivel_afiliacion = nivel_afiliacion;
 	}
 	public String getMetodo_pago() {
 		return metodo_pago;
